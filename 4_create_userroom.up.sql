cf9c9ee46fc848264342be70e1a494dceb885ebe
Phoebe Li <49734328+zpl0310@users.noreply.github.com>
2019-07-09 16:12:41 -0700

    Delete 4_create_userroom.up.sql

--- 4_create_userroom.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE userroom (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
-    room_id INTEGER REFERENCES rooms(id),
-    hasUnread boolean
-);
-
-CREATE INDEX ON userroom(user_id);
-CREATE INDEX ON userroom(room_id);
