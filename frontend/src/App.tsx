484f4b31c68e157fb686f6066c9511f3406f0e1e
jchou8 <jphchou@gmail.com>
2019-07-08 18:15:28 -0700

    move to frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
