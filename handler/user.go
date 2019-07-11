6f79fc60a11da6211bce863dbe3a3eafd307e5d5
Karen Bowman <kbowman@fetchrobotics.com>
2019-07-10 17:28:36 -0700

    working, but needs to authorize websocket with token not password/username

--- handler/user.go
+++ handler/user.go
@@ -23,7 +23,7 @@ type (
 
 	// TokenCredential is a payload that captures user submitted token
 	TokenCredential struct {
-		Username string `json:"jwtToken"`
+		jwtToken string `json:"jwtToken"`
 	}
 
 	// TokenResponse is a payload that returns JWT token back to client.
