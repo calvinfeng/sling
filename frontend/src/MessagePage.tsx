37dca84b42bbcd151b7cf02a4fbc044baca63486
jchou8 <jphchou@gmail.com>
2019-07-11 15:13:08 -0700

    move dispatch functions to separate file for better readability in messagepage

--- frontend/src/MessagePage.tsx
+++ frontend/src/MessagePage.tsx
@@ -11,7 +11,7 @@ import { AppActionTypes } from './actions/types'
 
 import { AppState } from './store'
 import { User, Room, Message } from './types'
-import curRoom from './reducers/curRoom';
+import { dispatchActions } from './store/dispatch'
 
 type MessagePageState = {
     inputEnabled: boolean
@@ -34,48 +34,7 @@ type OwnProps = {
 }
 
 const mapStateToProps = (state: AppState, ownProps: OwnProps) => ({ ...state, ...ownProps })
-const mapDispatchToProps = (dispatch: React.Dispatch<AppActionTypes>) => {
-    return {
-        onLogIn: (user: User) => {
-            dispatch(actions.logIn(user))
-        },
-        onLogOut: () => {
-            dispatch(actions.logOut())
-        },
-
-        onLoadMessages: (messages: Message[]) => {
-            dispatch(actions.loadMessages(messages))
-        },
-        onLoadRooms: (rooms: Room[]) => {
-            dispatch(actions.loadRooms(rooms))
-        },
-        onLoadUsers: (users: User[]) => {
-            dispatch(actions.loadUsers(users))
-        },
-
-        onNewMessage: (message: Message) => {
-            dispatch(actions.newMessage(message))
-        },
-        onNewRoom: (room: Room) => {
-            dispatch(actions.newRoom(room))
-        },
-        onNewUser: (user: User) => {
-            dispatch(actions.newUser(user))
-        },
-
-        onMarkUnread: (roomID: number) => {
-            dispatch(actions.markUnread(roomID))
-        },
-        onJoinRoom: (room: Room) => {
-            dispatch(actions.joinRoom(room))
-        },
-        onChangeRoom: (room: Room) => {
-            dispatch(actions.changeRoom(room))
-        },
-    }
-}
-
-type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof dispatchActions>
 
 class MessagePage extends React.Component<Props, MessagePageState> {
     private msgWebsocket: WebSocket
@@ -313,4 +272,4 @@ class MessagePage extends React.Component<Props, MessagePageState> {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MessagePage);
+export default connect(mapStateToProps, dispatchActions)(MessagePage);
